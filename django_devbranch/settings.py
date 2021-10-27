cffdf02e977bb4b94245acace80194f12b7e3f78
brandondoris <brandonmdoris@yahoo.com>
2021-10-27 19:56:22 -0400

    heroku deployment

--- django_devbranch/settings.py
+++ django_devbranch/settings.py
@@ -170,6 +170,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'devbranch-api'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
