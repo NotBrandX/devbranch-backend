2bcb2d9fe1838b399b86e1fa6541481d23087594
brandondoris <brandonmdoris@yahoo.com>
2021-10-30 20:32:29 -0400

    edited rest framwework settings

--- django_devbranch/settings.py
+++ django_devbranch/settings.py
@@ -145,10 +145,9 @@ REST_FRAMEWORK = {
         # Basic Authentication should be removed in production
         'rest_framework.authentication.BasicAuthentication',
     ],
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    # Use Django's Allow Any to allow all users to modify data
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ]
 }
 
